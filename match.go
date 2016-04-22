3788eb2fb9599dbef72a90b262130b0ed68e5248
SeeSpotRun <daniel.thomas@worleyparsons.com>
2016-04-23 07:54:14 +1000

    match: mimic rmlint CLI; use docopts; hash choice; file size limits

--- match.go
+++ match.go
@@ -22,7 +22,8 @@
 *
 **/
 
-// match is a demo main for the drs package.  It finds duplicate files based on sha1 checksums
+// match is a demo main for the drs package.  It finds duplicate files based on checksums.  It is
+// more or less a golang partial port of rmlint (https://github.com/sahib/rmlint)
 package main
 
 /*
@@ -32,7 +33,11 @@ package main
  */
 
 import (
-	"crypto/sha1"
+	"crypto"
+	_ "crypto/sha1"
+	_ "crypto/sha256"
+	_ "crypto/sha512"
+	_ "crypto/md5"
 	"hash"
 	"fmt"
 	"log"
@@ -40,13 +45,22 @@ import (
 	"sort"
 	"sync"
 	"runtime"
+	"strings"
 	"github.com/docopt/docopt-go"
+	"github.com/SeeSpotRun/coerce"
 	"github.com/SeeSpotRun/drs/drs"
 )
 
-//////////////////////////////////////////////////////////////////
+// tuning parameters
+const (
+	bufSize = 32 * 1024        // read buffer size
+	firstN = int64(bufSize)    // how many bytes in first hash increment
+	mult   = int64(7)          // acceleration factor
+	maxN   = int64(1) << 27    // maximum increment size = 128M
+	defaultHash = crypto.SHA1  // note: if change this then also change in usage string
+)
 
-type Hash [sha1.Size]byte
+//////////////////////////////////////////////////////////////////
 
 // File implements the drs.Job interface
 type File struct {
@@ -64,11 +78,6 @@ func (f *File) Schedule() {
 	f.disk.Schedule(f, f.path, f.offset, drs.Normal)
 }
 
-const (
-	firstN int64 = 32 * 1024
-	mult   int64 = 7
-	maxN   int64 = 32 * 1024 * 7 * 7 * 7 * 7
-)
 
 // Go hashes the file contents and adds the job to results[]
 func (f *File) Go(fd *drs.File, err error) {
@@ -116,7 +125,7 @@ const (
 type Group struct {
 	status   int
 	refs     int      // reference count (1 for incoming channel and 1 for each pending hash)
-	children map[Hash]*Group
+	children map[string]*Group
 	files    []*File
 	reportch chan <- *Group
 	grouper  *Grouper
@@ -125,7 +134,7 @@ type Group struct {
 func NewGroup(f *File, reportch chan <- *Group, grouper *Grouper) *Group {
 	g := &Group{
 		refs: 1,
-		children: make(map[Hash]*Group, 1),
+		children: make(map[string]*Group, 1),
 		reportch: reportch,
 		grouper:  grouper,
 	}
@@ -179,7 +188,7 @@ func (g *Group) unref() {
 	if g.refs < 0 {
 		panic("Group.unref yields negative reference count")
 	}
-	
+
 	if g.refs > 0 {
 		return
 	}
@@ -203,14 +212,33 @@ func NewGrouper() *Grouper {
 	return &Grouper { make(chan *File, 10) }
 }
 
+// mapable encodes []byte b to a uniquely mapable string.
+// Since strings can't contain 0 values it maps byte values as follows:
+// 0: {1, 1}
+// 1: {1, 2}
+// Encoding efficiency should average 99.2% (vs hex encoding 50%)
+func mapable(b[]byte) string {
+	j := 0 // start of current subslice
+	result := ""
+	for i := 0; i < len(b); i++ {
+		if b[i] <= 1 {
+			result = result + string(b[j:i]) + string(1) + string(b[i] + 1)
+			j = i + 1
+		}
+	}
+	return result + string(b[j:])
+}
+
 func (gr *Grouper) group() {
 	for f := range gr.filech {
 		g := f.group
 		if f.err != nil {
 			g.reportch <- &Group{files: []*File {f}, status: ReadError  }
 		} else {
-			var h Hash
-			copy(h[:], f.hash.Sum(nil)[:sha1.Size])
+			h := mapable( f.hash.Sum(nil) ) // []byte is not mapable but string is
+			if fmt.Sprintf("%x", h) != fmt.Sprintf("%x", f.hash.Sum(nil)) {
+				fmt.Printf("%x\n%x\n", f.hash.Sum(nil), h)
+			}
 			c, ok := g.children[h]
 			if !ok {
 				// create new group
@@ -262,7 +290,7 @@ func report_dupes(r *Reporter, g *Group) {
 func NewReporter() *Reporter {
 
 	r := &Reporter {reportch: make(chan *Group, 10) }
-	
+
 	go func() {
 		var reporters = []reporter {
 			report_unique,   // Dormant
@@ -276,7 +304,7 @@ func NewReporter() *Reporter {
 			g.files = nil
 		}
 	}()
-	
+
 	return r
 
 }
@@ -296,24 +324,85 @@ func main() {
     match -h | --help
     match [options] [--ssd=path]... [<paths>...]
 Options:
-  -h --help     Show this screen
-  --version     Show version
-  -r --recurse  Recurse paths if they are folders
-  --aggressive  Use more aggressive HDD scheduler settings
-  --ssd=path    Identify path as belonging to an SSD
+  -h --help              Show this screen
+rmlint-like options:
+  --maxDepth=N, -d N     Specify max traversal depth [default: -1]
+  --algorithm=A, -a A    Choose hash algorithm [default: SHA1]
+  --hidden, -r           Find hidden files and files in hidden folders
+  --followlinks, -f      Follow symlinks
+  --seelinks             Treat symlinks as little files and look for duplicate symlink
+  --xdev, -x             Do not cross mounpoints
+  --version              Show version
+  --readbuf=N            Use up to N bytes of read buffer [default: 32M]
+variations on rmlint options:
+  --minsize=m,  -s m     Specify minimum file size [default: 1]
+  --maxsize=M            Specify maximum file size [default: -1]
+non-rmlint options:
+  --ssd=path             Identify path as belonging to an SSD
+tuning options:
+  --aggressive           Use more aggressive HDD scheduler
 `
+
+	type options struct {
+		maxDepth	int
+		minsize		int64
+		maxsize		int64
+		algorithm	string
+		hidden		bool
+		followlinks	bool
+		xdev		bool
+		seelinks	bool
+		readbuf         int64
+		aggressive	bool
+		ssd		[]string
+		paths		[]string
+	}
+
+
 	args, _ := docopt.Parse(usage, os.Args[1:], true, "sums 0.1", false, true)
 
+	var opts options
+	err := coerce.Struct(&opts, args, "--%s", "-%s", "<%s>")
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("args:", args)
+		fmt.Println(usage)
+		return
+	}
+
+	hashtype := defaultHash
+	// hash type?
+	switch strings.ToUpper(opts.algorithm) {
+	case "SHA1":
+	case "SHA256":
+		hashtype = crypto.SHA256
+	case "SHA512":
+		hashtype = crypto.SHA512
+	case "MD5":
+		hashtype = crypto.MD5
+	default:
+		fmt.Println("Error: unrecognized hash algorithm:", opts.algorithm)
+		return
+	}
+
 	// use more aggressive scheduler for HDD's ?
-	if args["--aggressive"] == true {
+	if opts.aggressive {
 		drs.HDD = drs.Aggressive
 	}
+
+	drs.InitPool(bufSize, int( 1 + (opts.readbuf - 1) / bufSize ) )
+
 
 	// configure walk options
 	walkopts := &drs.WalkOptions{
 		Priority: drs.Normal,
 		Errs: make(chan error),
-		NoRecurse: args["--recurse"] != true,
+		MaxDepth: opts.maxDepth + 1,
+                HiddenFiles: opts.hidden,
+                HiddenDirs: opts.hidden,
+		SeeLinks: opts.seelinks || opts.followlinks,
+                FollowLinks: opts.followlinks,
+		OneDevice: opts.xdev,
 	}
 
 	// error reporting during walk:
@@ -324,32 +413,36 @@ Options:
 	}()
 
 	// register user-identified ssd's:
-	ssds, ok := args["--ssd"].([]string)
-	if ok {
-		err := drs.RegisterSSDs(ssds)
-		if err != nil {
-			fmt.Println("Error registering ssd's: %s", err)
-		}
+	err = drs.RegisterSSDs(opts.ssd)
+	if err != nil {
+		fmt.Println("Error registering ssd's: %s", err)
 	}
-	
-	var paths []string
-	paths, _ = args["<paths>"].([]string)
-	paths = append(paths, ssds...)
 
-	if len(paths) == 0 {
+	opts.paths = append(opts.paths, opts.ssd...)
+	if len(opts.paths) == 0 {
+		opts.paths = append(opts.paths, ".")
+	}
+
+	if len(opts.paths) == 0 {
 		fmt.Println("Error: no paths to hash")
 		return
 	}
 	// walk paths:
 	var files []*File
-	for p := range drs.Walk(paths, walkopts) {
+	for p := range drs.Walk(opts.paths, walkopts) {
+		if opts.minsize > 0 && p.Info.Size() < opts.minsize {
+			continue
+		}
+		if opts.maxsize >= 0 && p.Info.Size() > opts.maxsize {
+			continue
+		}
 		files = append(files,
 			&File{
 				path:   p.Name,
 				offset: p.Offset,
 				size:   uint64(p.Info.Size()),
 				disk:   p.Disk,
-				hash:   sha1.New(),
+				hash:   hashtype.New(),
 			},
 		)
 	}
@@ -361,7 +454,7 @@ Options:
 	grouper := NewGrouper()
 	
 	var g *Group
-	for _, f := range(files) {
+	for i, f := range(files) {
 		if g == nil || f.size != g.files[0].size {
 			// file doesn't fit current group
 			g.unref()
@@ -370,14 +463,16 @@ Options:
 			g.add(f)
 		}
 	}
+	// unref final group
 	g.unref()
+
 	runtime.GC()
 	
 	go grouper.group()
 
 	// wait for all jobs to finish
 	drs.Wait()
-	
+
 	// wait for printout to finish
 	reporter.wg.Wait()
 	fmt.Printf("Total %d duplicates of %d originals; total wasted bytes: %.2fGB\n", reporter.dupes, reporter.originals, float64(reporter.wasted) / 1024 / 1024 / 1024)
